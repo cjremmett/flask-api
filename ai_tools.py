94929c73d8642d0bf18bed808f1a936a8bd61222
cjremmett <cjremmett@gmail.com>
2025-04-28 16:37:05 -0400

    simulate ai processing delay

--- ai_tools.py
+++ ai_tools.py
@@ -1,6 +1,8 @@
 from flask import jsonify
+from time import sleep
 
 
 def get_dummy_message(): 
+    sleep(2)
     message = {"message": "Hello, this is a dummy message!"}
     return (jsonify(message), 200)
