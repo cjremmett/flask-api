7c0b9247429d466c437546e4278fb64d948bb079
cjremmett <cjremmett@gmail.com>
2025-05-01 15:28:11 -0400

    added some logging to the websocket functions

--- ai_tools.py
+++ ai_tools.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from time import sleep
 from flask_socketio import send, emit
+from utils import append_to_log
 
 # Need this to support socketio decorators
 # Docs say we need to import from __main__, but with gunicorn __name__ == 'main'
@@ -15,5 +16,12 @@ def get_dummy_message():
 
 @socketio.on('json')
 def handle_test_websocket_message(json):
+    append_to_log('flask_logs', 'AI', 'DEBUG', str(json))
     resp_json = {'message': 'This is a dummy response. Your message was: ' + json['message']}
     send(resp_json, json=True)
+
+
+@socketio.on('message')
+def handle_message(data):
+    append_to_log('flask_logs', 'AI', 'DEBUG', str(data))
+    send(data + ' Hello world!')
